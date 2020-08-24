8ebc651b8cec835278ff0fdf66401cdd4858de28
Iro <felipemnomura@gmail.com>
2020-08-23 21:00:37 -0300

    Classe Catalogo implementada

--- Catalogo.java
+++ Catalogo.java
@@ -1,1 +1,32 @@
 //teste aaaaaaaaaaaaaaaaaa
+public class Catalogo
+{
+    String Id;
+    String cor;
+    int Ano;
+    String Pais;
+
+    public Catalogo(String id, String cor, int Ano, String pais)
+    {
+        this.Id = id;
+        this.cor =  cor;
+        this.Ano = Ano;
+        this.Pais = pais;
+    }
+    public String getId()
+    {
+        return Id;
+    }
+    public String getCor()
+    {
+        return cor;
+    }
+    public int getAno()
+    {
+        return Ano;
+    }
+    public String getPais()
+    {
+        return Pais;
+    }
+}
